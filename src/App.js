8a77c64434b8627863001f9b531d07065ede1dc7
sagrikaa <40899302+sagrikaa@users.noreply.github.com>
2019-11-01 15:23:09 -0400

    final upload

--- src/App.js
+++ src/App.js
@@ -1,62 +1,72 @@
-import React ,{Component} from 'react';
+// Note Taking Express Challenge 1  
+//@Author Sagrika Aggarwal
+  
+
+import React , {Component} from 'react';
 import axios from 'axios'
+import { tsInterfaceBody } from '@babel/types';
 
 class App extends Component {
   
   state = { 
     name:'',
     email:'',
-    message:'',
-    test:'',
-    host:'',
-    password:'',
+    body:''
     }
 
   onChange = e => this.setState({[e.target.name] : e.target.value});     
 
   onSubmit = (e)=>{
      e.preventDefault()
-     console.log(this.state)
      const headers = { 'Content-Type':'application/json'}
-
      axios.post(`https://cors-anywhere.herokuapp.com/https://ey8ola9nmf.execute-api.us-east-2.amazonaws.com/hiring`,this.state, {headers:headers}).then(res=>
-     {
-              console.log(res.data.body)
-              this.setState({
-                test:res.data.body.test,
-                host:res.data.body.host,
-                message:res.data.body.message
-              }) 
-             
-      
+     { alert('Congratulations for completing Stage 1')
+       this.setState({ body:res.data.body}) 
      })
   }
 
  render(){
+   const {body} = this.state;
   return (
     <div className="App" >
-    <div className='card col-md-8 offset-md-2 mt-4'>
-      
 
-    <form className="form m-2" onSubmit={this.onSubmit}>
-                    
-                <div className='m-4'>
-                <input className="form-control m-3" type="text" placeholder="Name"  name ='name' aria-label="Search"  value={this.state.name} onChange={this.onChange}/>
-                </div>
-                <div className='m-4'>
-                <input className="form-control m-3" type="text" placeholder="Email"  name ='email' aria-label="Search"  value={this.state.email} onChange={this.onChange}/>
-                </div>
-                <button className=" btn blue-gradient m-3 btn-md" type="submit" style={{color:'white'}}>Send</button>
-                <div className='m-4'>
-                  <label>{this.state.message}</label><br></br>
-                  <label>{this.state.host}</label><br></br>
-                  <label>{this.state.test}</label><br></br>
-                  <label>{this.state.password}</label><br></br>
+      <div className='col-md-4 offset-md-4' style={{marginTop:'50px'}}>
 
-                </div>
-                
-    </form>
-     </div>
+          <div className="card-header" style={{backgroundColor:'black',color:'white'}}>
+            NOTE TAKING EXPRESS STAGE 1
+          </div>
+
+          <div className='card-body' >
+            <form className="form" onSubmit={this.onSubmit}>
+                            
+                        <div className='m-3'>
+                          <label>Name</label>
+                        <input className="form-control" type="text" placeholder="Name"  name ='name' aria-label="Search"  value={this.state.name} onChange={this.onChange}/>
+                        </div>
+
+                        <div className='m-3'>
+                        <label>Email</label>
+                        <input className="form-control" type="text" placeholder="Email"  name ='email' aria-label="Search"  value={this.state.email} onChange={this.onChange}/>
+                        </div>
+
+                        <center><button className="btn m-3" type="submit" style={{color:'white',backgroundColor:'#3B9BCF'}}>Send</button></center>
+                      
+                      {
+                        body?<div className='m-4'>
+                          <label ><b>Message:</b> {body.message}</label><br></br>
+                          <label><b>Test:</b> {body.test}</label><br></br>
+                          <label><b>Credentials:</b></label><br></br>
+                          <label><b>Host:</b></label>{body.host}<br></br>
+                          <label><b>UserName:</b></label>{body.host}<br></br>
+                          <label><b>Password:</b></label>{body.password}<br></br>
+                          
+                        </div>:null
+                      } 
+                        
+            </form>
+          </div>
+     
+      </div>
     </div>
   );
  }
